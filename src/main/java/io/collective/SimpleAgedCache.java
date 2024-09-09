293428d6127e8cd40ad446fccdfd14369a480dba
melalex <alexander.melashchenko@gmail.com>
2024-09-10 00:14:51 +0300

    Implement SimpleAgedCache

--- src/main/java/io/collective/SimpleAgedCache.java
+++ src/main/java/io/collective/SimpleAgedCache.java
@@ -1,27 +1,57 @@
 package io.collective;
 
 import java.time.Clock;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SimpleAgedCache {
 
+    private final Clock clock;
+    private final Map<Object, CacheEntry> cacheEntryMap = new HashMap<>();
+
     public SimpleAgedCache(Clock clock) {
+        this.clock = clock;
     }
 
     public SimpleAgedCache() {
+        this(Clock.systemUTC());
     }
 
     public void put(Object key, Object value, int retentionInMillis) {
+        var now = clock.instant();
+
+        cacheEntryMap.put(key, new CacheEntry(value, now.plusMillis(retentionInMillis)));
     }
 
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     public int size() {
-        return 0;
+        var now = clock.instant();
+
+        return (int) cacheEntryMap.values().stream()
+                .filter(it -> it.validTo.isAfter(now))
+                .count();
     }
 
     public Object get(Object key) {
-        return null;
+        var now = clock.instant();
+        var value = cacheEntryMap.get(key);
+
+        if (value == null) {
+            return null;
+        } else if (value.validTo.isAfter(now)) {
+            return value.value;
+        } else {
+            return null;
+        }
+    }
+
+    private record CacheEntry(
+            Object value,
+            Instant validTo
+    ) {
     }
 }
--- src/main/kotlin/io/collective/SimpleAgedKache.kt
+++ src/main/kotlin/io/collective/SimpleAgedKache.kt
@@ -1,27 +1,51 @@
 package io.collective
 
 import java.time.Clock
+import java.time.Instant
 
 class SimpleAgedKache {
+
+    private val clock: Clock
+    private val cacheEntryMap: MutableMap<Any?, CacheEntry> = HashMap()
+
     constructor(clock: Clock?) {
+        this.clock = clock ?: Clock.systemUTC()
     }
 
-    constructor() {
+    constructor() : this(null) {
+
     }
 
     fun put(key: Any?, value: Any?, retentionInMillis: Int) {
+        val now = clock.instant()
 
+        cacheEntryMap[key] = CacheEntry(value, now.plusMillis(retentionInMillis.toLong()))
     }
 
     fun isEmpty(): Boolean {
-        return false
+        return size() == 0
     }
 
     fun size(): Int {
-        return 0
+        val now = clock.instant()
+
+        return cacheEntryMap.values.stream()
+            .filter { it.validTo.isAfter(now) }
+            .count()
+            .toInt()
     }
 
     fun get(key: Any?): Any? {
-        return null
+        val now = clock.instant()
+        val value = cacheEntryMap[key]
+
+        return if (value == null) null
+        else if (value.validTo.isAfter(now)) value.value
+        else null
     }
+
+    private data class CacheEntry(
+        val value: Any?,
+        val validTo: Instant
+    )
 }
